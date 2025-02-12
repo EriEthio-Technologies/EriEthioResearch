16c4285ed4721ec9971c8d4a682381fc9998f2a9
Yosuf Nawed <yosuf.nawed@example.com>
2025-02-11 21:16:21 -0600

    chore: update project configuration and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,16 @@ const nextConfig = {
   },
   images: {
     domains: ['flvhpuholufzxclylrvh.supabase.co'],
+    unoptimized: true,
   },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  }
+  },
+  output: 'export',
+  trailingSlash: true,
 }
 
 export default nextConfig
